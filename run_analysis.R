51f3a55e4f99c6625eb1ac98ac205e4c89daf2aa
arun404 <arun.vonoz@gmail.com>
2014-06-22 20:33:54 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -20,18 +20,15 @@ run_analysis <- function() {
   ##Load Activity Features into datset
   data_set$activity_labels <- read.table(paste(run_analysis_folder, "activity_labels.txt", sep="/"), col.names=c('id', 'Activity'))
   
-  
   ##Load Test Dataset
   data_set$test <- cbind(subject=read.table(paste(run_analysis_folder, "test", "subject_test.txt", sep="/"), col.names="Subject"),
                          y=read.table(paste(run_analysis_folder, "test", "y_test.txt", sep="/"), col.names="Activity.ID"),
                          x=read.table(paste(run_analysis_folder, "test", "x_test.txt", sep="/")))
   
-  
   ##Load Train Dataset
   data_set$train <- cbind(subject=read.table(paste(run_analysis_folder, "train", "subject_train.txt", sep="/"), col.names="Subject"),
                           y=read.table(paste(run_analysis_folder, "train", "y_train.txt", sep="/"), col.names="Activity.ID"),
                           x=read.table(paste(run_analysis_folder, "train", "X_train.txt", sep="/")))
-  
   
   ## Extracts only the measurements on the mean and standard deviation for each measurement
   tidy_dataset <- rbind(data_set$test, data_set$train)[,c(1, 2, grep("mean\\(|std\\(", data_set$features$name) + 2)]
@@ -39,18 +36,15 @@ run_analysis <- function() {
   ## Uses descriptive activity names
   names(tidy_dataset) <- c("Subject", "Activity.ID", replace_func(data_set$features$name[grep("mean\\(|std\\(", data_set$features$name)]))
   
-  
   ## Merges data sets
   tidy_dataset <- merge(tidy_dataset, data_set$activity_labels, by.x="Activity.ID", by.y="id")
   tidy_dataset <- tidy_dataset[,!(names(tidy_dataset) %in% c("Activity.ID"))]
   
   ## Wite Output file
   write.csv(tidy_dataset, file = "tidy_dataset.txt",row.names = FALSE)
-  
 }
 
-
-
+##Function to replace headers with the meaningful header names
 replace_func <- function(col_name) {
   col_name <- gsub("tBody", "Time.Body", col_name)
   col_name <- gsub("tGravity", "Time.Gravity", col_name)
@@ -61,7 +55,5 @@ replace_func <- function(col_name) {
   col_name <- gsub("\\-mean\\(\\)", ".Mean", col_name)
   col_name <- gsub("\\-std\\(\\)", ".Std", col_name)
   
-  
   return(col_name)
 }
-
